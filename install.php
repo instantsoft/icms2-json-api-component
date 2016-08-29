77cec978e230b244f98e716e55678aac6b99827b
fuze <fuze@localhost>
2016-08-30 01:55:15 +0300

    init

---
+++ install.php
@@ -0,0 +1,13 @@
+<?php
+
+function install_package(){
+
+	return true;
+
+}
+
+function after_install_package(){
+
+    return true;
+
+}
---
+++ package/languages/ru/controllers/api/api.php
@@ -0,0 +1,2 @@
+<?php
+define('LANG_API_CONTROLLER', 'InstantCMS JSON API');
